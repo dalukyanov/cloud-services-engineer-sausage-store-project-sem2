768eb6df9fcce2ca0117370bd38d07c83ccf4f49
dalukyanov <hanatsu.tori@gmail.com>
2026-09-15 13:54:44 +0300

    Add: Flyway migrations

---
+++ backend/src/main/resources/migrations/V004__create_index.sql
@@ -0,0 +1,11 @@
+CREATE INDEX CONCURENTLY IF NOT EXISTS idx_orders_status_date_created
+    ON orders (status, date_created);
+
+CREATE INDEX CONCURENTLY IF NOT EXISTS idx_orders_date_created
+    ON orders (date_created);
+
+CREATE INDEX CONCURENTLY IF NOT EXISTS idx_order_product_order_id
+    ON order_product (order_id);
+
+CREATE INDEX CONCURENTLY IF NOT EXISTS idx_order_product_product_id
+    ON order_product (product_id);
